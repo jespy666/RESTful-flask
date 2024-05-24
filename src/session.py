3ba79775d3704e88f89c7f7b15e83436b2e5c800
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2024-05-24 20:41:26 +0500

    use flask-restful

--- src/session.py
+++ src/session.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
 
+
 class SessionFactory:
+
     def __init__(self, db_url: str) -> None:
         self.engine = create_engine(db_url)
         self.session_factory = sessionmaker(
--- src/tasks/routes.py
+++ src/tasks/routes.py
@@ -1,90 +1,83 @@
-from typing import Tuple, Sequence
+from typing import Sequence, Any
 
-from flask import Blueprint, request, jsonify, Response
+from flask import request, jsonify, Response
+from flask_restful import Resource
 
 from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 
 from src.tasks.models import Task
 from src.session import SessionFactory
-
-from config import app_config
-
-
-class TasksBlueprint(SessionFactory):
-
-    def __init__(self) -> None:
-        db_url: str = app_config.get_connection_uri()
-        super().__init__(db_url)
-        self.tasks_bp = Blueprint('tasks', __name__)
-        self.set_urls()
-
-    def create_task(self) -> Tuple[Response, int]:
-        if request.method == 'POST':
-            data: dict = request.json
-            task = Task(
-                title=data.get('title'),
-                description=data.get('description')
-            )
-            session: Session = super().get_session()
-            session.add(task)
-            session.commit()
-            session.close()
-            return jsonify(task.to_dict()), 201
-
-    def get_tasks(self) -> Tuple[Response, int]:
-        if request.method == 'GET':
-            session: Session = super().get_session()
-            stmt = select(Task)
-            tasks: Sequence[Task] = session.scalars(stmt).all()
-            session.close()
-            return jsonify([task.to_dict() for task in tasks]), 200
-
-    def get_task(self, id: int) -> Tuple[Response, int]:
-        if request.method == 'GET':
-            session: Session = super().get_session()
-            stmt = select(Task).where(Task.id == id)
-            task: Task | None = session.scalar(stmt)
-            session.close()
-            if task:
-                return jsonify(task.to_dict()), 200
-
-    def update_task(self, id: int) -> Tuple[Response, int]:
-        if request.method == 'PUT':
-            params: dict = request.json
-            session: Session = super().get_session()
-            stmt = update(Task).where(Task.id == id).values(**params)
-            session.execute(stmt)
-            session.commit()
-            task = session.query(Task).get(id)
-            session.close()
-            return jsonify(task.to_dict()), 200
-
-    def delete_task(self, id: int) -> Tuple[Response, int]:
-        if request.method == 'DELETE':
-            session: Session = super().get_session()
-            stmt = delete(Task).where(Task.id == id)
-            session.execute(stmt)
-            session.commit()
-            session.close()
-            return jsonify({"message": "Task was delete"}), 200
-
-    def set_urls(self) -> None:
-        self.tasks_bp.add_url_rule(
-            '/<int:id>', view_func=self.update_task, methods=['PUT']
-        )
-        self.tasks_bp.add_url_rule(
-            '/<int:id>', view_func=self.delete_task, methods=['DELETE']
-        )
-        self.tasks_bp.add_url_rule(
-            '/<int:id>', view_func=self.get_task, methods=['GET']
-        )
-        self.tasks_bp.add_url_rule(
-            '/', view_func=self.create_task, methods=['POST']
-        )
-        self.tasks_bp.add_url_rule(
-            '/', view_func=self.get_tasks, methods=['GET']
-        )
+from src.tasks.validators import validate_task
 
 
-tasks_bp = TasksBlueprint().tasks_bp
+class DirectTasks(Resource, SessionFactory):
+
+    def __init__(self, **kwargs) -> None:
+        db_url: str = kwargs.get('db_url')
+        super().__init__(db_url=db_url)
+
+    def get(self) -> Response:
+        session: Session = super().get_session()
+        stmt = select(Task)
+        tasks: Sequence[Task] = session.scalars(stmt).all()
+        session.close()
+        return jsonify([task.to_dict() for task in tasks])
+
+    def post(self) -> Response:
+        data: dict = request.json
+        session: Session = super().get_session()
+        if not validate_task(data):
+            return jsonify({"error": "validation error"})
+        stmt = select(Task).where(Task.title == data.get('title'))
+        task: Task | None = session.scalar(stmt)
+        if task:
+            return jsonify({"error": "task with that title are already exists"})  # noqa: E501
+        new_task = Task(**data)
+        session.add(new_task)
+        session.commit()
+        session.close()
+        return jsonify(new_task.to_dict())
+
+
+class ParametrizeTasks(Resource, SessionFactory):
+
+    def __init__(self, **kwargs: Any) -> None:
+        db_url: str = kwargs.get('db_url')
+        super().__init__(db_url=db_url)
+
+    def get(self, task_id: int) -> Response:
+        session: Session = super().get_session()
+        stmt = select(Task).where(Task.id == task_id)
+        task: Task | None = session.scalar(stmt)
+        session.close()
+        if task:
+            return jsonify(task.to_dict())
+        return jsonify({"error": "task does not exist"})
+
+    def put(self, task_id: int) -> Response:
+        params: dict = request.json
+        if not validate_task(params):
+            return jsonify({"error": "validation error"})
+        session: Session = super().get_session()
+        stmt1 = select(Task).where(Task.id == task_id)
+        task: Task | None = session.scalar(stmt1)
+        if not task:
+            return jsonify({"error": "task does not exist"})
+        stmt2 = update(Task).where(Task.id == task_id).values(**params)
+        session.execute(stmt2)
+        session.commit()
+        session.close()
+        return jsonify(task.to_dict())
+
+    def delete(self, task_id: int) -> Response:
+        session: Session = super().get_session()
+        stmt1 = select(Task).where(Task.id == task_id)
+        task: Task | None = session.scalar(stmt1)
+        if not task:
+            return jsonify({"error": "task does not exist"})
+        stmt2 = delete(Task).where(Task.id == task_id)
+        session.execute(stmt2)
+        session.commit()
+        session.close()
+        return jsonify({"message": "Task was delete"})
